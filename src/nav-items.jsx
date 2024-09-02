8327835791124e6d336a4f04f604461939355d51
Muhamad Said Fathurrohman <muh.said@gmail.com>
2024-09-02 23:02:30 +0700

    activate decap cms vite plugin

--- src/nav-items.jsx
+++ src/nav-items.jsx
@@ -1,6 +1,8 @@
 import { HomeIcon, PenIcon, BookOpenIcon, SettingsIcon, GraduationCapIcon, HeartHandshakeIcon, ScaleIcon } from "lucide-react";
 import Index from "./pages/Index.jsx";
 import BlogPosts from "./pages/BlogPosts.jsx";
+import { SettingsIcon } from "lucide-react";
+import CMS from "./pages/CMS.jsx";
 
 /**
  * Central place for defining the navigation items. Used for navigation components and routing.
@@ -17,5 +19,11 @@ export const navItems = [
     to: "/blog",
     icon: <BookOpenIcon className="h-4 w-4" />,
     page: <BlogPosts />,
+  },
+  {
+    title: "CMS",
+    to: "/admin",
+    icon: <SettingsIcon className="h-4 w-4" />,
+    page: <CMS />,
   },
 ];
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { defineConfig } from 'vite'
+
+import react from '@vitejs/plugin-react'
+
+import { VitePluginDecapCMS } from 'vite-plugin-decap-cms'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +14,18 @@ export default defineConfig({
     host: "::",
     port: "8080",
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginDecapCMS({
+      config: {
+        backend: {
+          name: 'github',
+          repo: 'mudamerdeka/mudamerdeka.github.io',
+          branch: 'main',
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: [
       {
