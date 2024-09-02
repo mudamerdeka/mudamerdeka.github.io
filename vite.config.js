d9ea67c57f84e415d4deff62227464f184dafc74
Muhamad Said Fathurrohman <muh.said@gmail.com>
2024-09-02 23:21:55 +0700

    actvate vie decap cms plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import decapCmsPlugin from "vite-plugin-decap-cms";
+import { resolve } from "path";
+import { defineConfig } from 'vite'
 
+import react from '@vitejs/plugin-react'
+
+import { VitePluginDecapCMS } from 'vite-plugin-decap-cms'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), decapCmsPlugin()],
+  server: {
+    host: "::",
+    port: "8080",
+  },
+  plugins: [
+    react(),
+    VitePluginDecapCMS({
+      config: {
+        backend: {
+          name: 'github',
+          repo: 'mudamerdeka/mudamerdeka.github.io',
+          branch: 'main',
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      {
+        find: "lib",
+        replacement: resolve(__dirname, "lib"),
+      },
+    ],
+  },
 });
